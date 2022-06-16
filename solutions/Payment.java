1b08e885dfc1b3d5749e2484ff95b7057dc83dad
pepeijal <103349590+pepeijal@users.noreply.github.com>
2022-06-16 23:34:43 +0800

    Update Payment.java

--- solutions/Payment.java
+++ solutions/Payment.java
@@ -25,7 +25,7 @@ public class Payment {
         long currenttime = 0;
 
         try{
-            Scanner in = new Scanner(new FileInputStream("C:\\Users\\Ideapad 3\\Downloads\\DS-Assignment-2022-main\\DS-Assignment-2022-main\\tasks\\payment\\cases\\1.txt"));
+            Scanner in = new Scanner(new FileInputStream("1.txt"));
             while(in.hasNextLine()){
                 long noww = System.currentTimeMillis();
                 String line = in.nextLine();
@@ -94,5 +94,65 @@ public class Payment {
         for(IDs y : x){
             y.plusTime(z);
         }
+    }
+}
+class IDs implements Comparable<IDs>{
+
+    private String id;
+    private String id1;
+    private String prio;
+    private long time;
+
+    public IDs(String x){
+        String[] split = x.split(" ");
+        this.id = split[0];
+        this.id1 = split[1];
+        this.prio = split[2];
+        switch(this.prio){
+            case "PLATINUM":{
+                this.time = 3000;
+                break;
+            }
+            case "GOLD":{
+                this.time=2000;
+                break;
+            }
+            case "SILVER":{
+                this.time=1000;
+                break;
+            }
+            case "BRONZE":{
+                this.time=0;
+                break;
+            }
+
+        }
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void plusTime(long x) {
+        this.time += x;
+    }
+
+    @Override
+    public String toString() {
+        return "("+id1+" "+prio+") --> ";
+    }
+
+    @Override
+    public int compareTo(IDs o) {
+        if(this.time>o.time){
+            return 1;
+        }else if(this.time < o.time){
+            return -1;
+        }else
+        return 0;
+    }
+
+    public void print(){
+        System.out.print(this.id1+" ");
     }
 }
