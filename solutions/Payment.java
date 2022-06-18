7ff7b224bb0abc55922cd4a8eedebe8d901e7716
pepeijal <103349590+pepeijal@users.noreply.github.com>
2022-06-18 19:35:23 +0800

    Update Payment.java

--- solutions/Payment.java
+++ solutions/Payment.java
@@ -1,149 +1,196 @@
-import java.io.FileInputStream;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 
+
+
 public class Payment {
-    public static void main(String[] args){
-
-        ArrayList<IDs> ids = new ArrayList<>();
-        PriorityQueue<IDs> que = new PriorityQueue<>(Collections.reverseOrder());
-        ArrayList<IDs> between = new ArrayList<>();
-        long miliseconds = 1000;
-        long time = System.currentTimeMillis();
-        long currenttime = 0;
-
-        try{
-            Scanner in = new Scanner(System.in);
-            while(in.hasNextLine()){
-                long noww = System.currentTimeMillis();
-                String line = in.nextLine();
-                if(line.equals("CLEAR")){
-                    String out = in.nextLine();
-                    String[] split = out.split(" ");
-                    System.out.print("\n\nOUTPUT CLEAR :");
-                    print10(split);
-                    System.out.println("\n\n");
-                    System.out.print("QUE "+que);
-
-                    que.clear();
-                    System.out.println(que);
-                }else if(line.equals("END")){
-                    long end = System.currentTimeMillis();
-                    long total = end-time;
-                    System.out.println(total+" "+ids.get(0).getTime());
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PriorityQueue<Transactions> queue_transaction = new PriorityQueue<>();
+        long second_digit1;
+        long second_digit2 = 0;
+        boolean isClear = false;
+        while(in.hasNextLine()){
+                try{
+                    String input_transaction = in.nextLine();
+                    if(input_transaction.equals("EXIT") || input_transaction.equals("REBOOT")){
+                        if(input_transaction.equals("EXIT")){
+                            break;
+                        }
+                        if(input_transaction.equals("REBOOT")){
+                            queue_transaction.clear();
+                        }
+                        
+                    }else{
+                        String[] transaction_details = input_transaction.split(" ");
+                        long transaction_as_long = Long.valueOf(transaction_details[0]);
+                        second_digit1 = transaction_as_long;
+                        if(queue_transaction.isEmpty()){
+                            second_digit2 = transaction_as_long;
+                        }
+                    
+//                        if(second_digit1 != second_digit2){
+//                            second_digit2 = transaction_as_long/1000;
+//                            int i = 0;
+//                            String[] joined_id;
+//                            Long[] joined_epoch;
+//                            if(!queue_transaction.isEmpty()){
+//                                joined_epoch = new Long[100];
+//                                joined_id = new String[100];
+//                                while(i!=100){
+////                            while(!queue_transaction.isEmpty()){
+////                                    joined_epoch[i] = queue_transaction.peek().getEpoch_time_ms( );
+//                                    joined_id[i] = queue_transaction.poll().getTxn_id();
+//                                    i++;
+////                                if(i==100)break;
+//                                }
+//                                String hundredths_transaction = String.join(" ", joined_id);
+//                                System.out.println(hundredths_transaction);
+////                            for(int j = 0; j<joined_id.length ; j++){
+////                                System.out.println(j + " " + joined_epoch[j] + " " + joined_id[j]);
+////                            }
+//                            } 
+//                        }
+                    Transactions meowtransaction = new Transactions(transaction_as_long,transaction_details[1],transaction_details[2]);
+                            switch(transaction_details[2]){
+                                case "BRONZE":
+                                    queue_transaction.offer(meowtransaction);
+                                    break;
+                                case "SILVER":
+                                    meowtransaction.setEpoch_time_ms(meowtransaction.getEpoch_time_ms()-1000);
+                                    queue_transaction.offer(meowtransaction);
+                                    break;
+                                case "GOLD":
+                                    meowtransaction.setEpoch_time_ms(meowtransaction.getEpoch_time_ms()-2000);
+                                    queue_transaction.offer(meowtransaction);
+                                    break;
+                                case "PLATINUM":
+                                    meowtransaction.setEpoch_time_ms(meowtransaction.getEpoch_time_ms()-3000);
+                                    queue_transaction.offer(meowtransaction);
+                                    break;
+                                default:
+                                    break;
+                        }
+                        
+                        if(second_digit1/1000 != second_digit2/1000 && second_digit1 > second_digit2/1000*1000+1000){//second_digit1 > second_digit2 //second_digit1 != second_digit2
+//                            System.out.println(second_digit1);
+//                            System.out.println(second_digit2);
+//                            System.out.println((second_digit2/1000)*1000);
+                            second_digit2 = transaction_as_long;
+                            int i = 0;
+                            String[] joined_id;
+                            Long[] joined_epoch;
+                            if(!queue_transaction.isEmpty()){
+                                if(queue_transaction.size()>=100){
+                                    joined_epoch = new Long[100];
+                                    joined_id = new String[100];
+                                    while(!queue_transaction.isEmpty()){
+                                        joined_epoch[i] = queue_transaction.peek().getEpoch_time_ms( );
+                                        joined_id[i] = queue_transaction.poll().getTxn_id();
+                                        i++;
+                                        if(i==100)break;
+                                    }
+                                }else{
+                                    joined_epoch = new Long[queue_transaction.size()];
+                                    joined_id = new String[queue_transaction.size()];
+                                    while(!queue_transaction.isEmpty()){
+                                        joined_epoch[i] = queue_transaction.peek().getEpoch_time_ms( );
+                                        joined_id[i] = queue_transaction.poll().getTxn_id();
+                                        i++;
+                                        if(i==100)break;
+                                    }
+                                }
+                                String hundredths_transaction = String.join(" ", joined_id);
+                                System.out.println(hundredths_transaction);
+//                            for(int j = 0; j<joined_id.length ; j++){
+//                                System.out.println(j + " " + joined_epoch[j] + " " + joined_id[j]);
+//                            }
+                            }
+                        }  
+                    }
+                    
+                }catch(Exception e){
                     return;
                 }
-                else if(currenttime >= miliseconds){
-                    miliseconds += 1000;
-                    System.out.print("\nOUTPUT : ");
-                    int numb = 0;
-                    for(IDs x : between){
-                        if(numb%5==0 && numb !=0)
-                            System.out.println();
-                        x.print();
-                        numb++;
+        }
+    }
+}        
+
+    class Transactions<E> implements Comparable<Transactions>{
+    private Long epoch_time_ms;
+    private String txn_id;
+    private String tier;
+    private Integer tier_int;
+
+    public Transactions(Long epoch_time_ms, String txn_id, String tier) {
+        this.epoch_time_ms = epoch_time_ms;
+        this.txn_id = txn_id;
+        this.tier = tier;
+        
+        switch(this.tier){
+                        case "BRONZE":
+                            tier_int = 0;
+                            break;
+                        case "SILVER":
+                            tier_int = 1;
+                            break;
+                        case "GOLD":
+                            tier_int = 2;
+                            break;
+                        case "PLATINUM":
+                            tier_int = 3;
+                            break;
+                        default:
+                            break;
                     }
-                    between.clear();
+    }
 
-                    System.out.println("\n\nQUE: "+que);
-                }
+    public Integer getTier_int() {
+        return tier_int;
+    }
 
-                else {
-                    long before = System.currentTimeMillis();
-                    Thread.sleep(10);
-                    long after = System.currentTimeMillis();
-                    long timebet = after-before;
-                    currenttime += timebet + (after - noww);
-                    addTime(ids,timebet);
-                    between.add(new IDs(line));
-                    ids.add(new IDs(line));
-                    que.add(new IDs(line));
-                    System.out.println("INPUT  :" + line);
-                }
-            }
+    public void setTier_int(int tier_int) {
+        this.tier_int = tier_int;
+    }
 
-            in.close();
+    public Long getEpoch_time_ms() {
+        return epoch_time_ms;
+    }
 
-        }catch (Exception e){
-            System.out.println("Not Found");
+    public void setEpoch_time_ms(Long epoch_time_ms) {
+        this.epoch_time_ms = epoch_time_ms;
+    }
+
+    public String getTxn_id() {
+        return txn_id;
+    }
+
+    public void setTxn_id(String txn_id) {
+        this.txn_id = txn_id;
+    }
+
+    public String getTier() {
+        return tier;
+    }
+
+    public void setTier(String tier) {
+        this.tier = tier;
+    }
+    
+    
+    @Override
+    public int compareTo(Transactions t) {
+        if(this.getEpoch_time_ms().compareTo(t.getEpoch_time_ms())==0){
+            return this.getTier_int().compareTo(t.getTier_int());
         }
-    }
-
-    public static void print10(String[] x){
-        for(int i = 0;i < x.length;i++){
-            if(i%9==0 && i!=0){
-                System.out.println();
-            }
-            System.out.print(x[i]+" ");
-        }
-    }
-
-    public static void addTime(ArrayList<IDs> x,long z){
-        for(IDs y : x){
-            y.plusTime(z);
-        }
-    }
-}
-class IDs implements Comparable<IDs>{
-
-    private String id;
-    private String id1;
-    private String prio;
-    private long time;
-
-    public IDs(String x){
-        String[] split = x.split(" ");
-        this.id = split[0];
-        this.id1 = split[1];
-        this.prio = split[2];
-        switch(this.prio){
-            case "PLATINUM":{
-                this.time = 3000;
-                break;
-            }
-            case "GOLD":{
-                this.time=2000;
-                break;
-            }
-            case "SILVER":{
-                this.time=1000;
-                break;
-            }
-            case "BRONZE":{
-                this.time=0;
-                break;
-            }
-
-        }
-    }
-
-    public long getTime() {
-        return time;
-    }
-
-    public void plusTime(long x) {
-        this.time += x;
+        return this.getEpoch_time_ms().compareTo(t.getEpoch_time_ms());
     }
 
     @Override
     public String toString() {
-        return "("+id1+" "+prio+") --> ";
+        return "time : " + epoch_time_ms + "ID :  "+ txn_id + "\n";
     }
-
-    @Override
-    public int compareTo(IDs o) {
-        if(this.time>o.time){
-            return 1;
-        }else if(this.time < o.time){
-            return -1;
-        }else
-        return 0;
-    }
-
-    public void print(){
-        System.out.print(this.id1+" ");
-    }
+    
+    
 }
